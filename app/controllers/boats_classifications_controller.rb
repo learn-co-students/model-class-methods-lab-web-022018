0458e5898712ce7a1b906531d3ada733f6d0fc8b
Ray Blasko <mray6288@gmail.com>
2018-03-21 16:54:42 -0400

    Done.

---
+++ app/controllers/boats_classifications_controller.rb
@@ -0,0 +1,2 @@
+class BoatsClassificationsController < ApplicationController
+end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+  	joins(boats: :classifications).where(classifications:{name:'Catamaran'})
+  end
+
+  def self.sailors
+  	joins(boats: :classifications).where(classifications:{name:'Sailboat'}).group(:captain_id)
+  end
+
+  def self.talented_seafarers
+  	sail = self.sailors
+  	motor = joins(boats: :classifications).where(classifications:{name:'Motorboat'}).group(:captain_id)
+  	where('id in (?)', sail & motor)
+  end
+
+  def self.non_sailors
+  	where.not('id in (?)', self.sailors.pluck(:id))
+  end
+
+  
+
 end
